459e667f9b78954c44a981040a10dd3329535b62
MinatoFanboy <thanh31299@gmail.com>
2024-08-21 10:32:59 +0700

    Send & Received Message

--- app/(app)/chatRoom.tsx
+++ app/(app)/chatRoom.tsx
@@ -1,20 +1,92 @@
-import React, { FC, useState } from 'react';
-import { TextInput, TouchableOpacity, View } from 'react-native';
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { Alert, TextInput, TouchableOpacity, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useLocalSearchParams, useRouter } from 'expo-router';
+import { addDoc, collection, doc, onSnapshot, orderBy, query, setDoc, Timestamp } from 'firebase/firestore';
 import { Feather } from '@expo/vector-icons';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 import { ChatRoomHeader, CustomKeyboard, MessageList } from '@/components';
+import { useAuth } from '@/context/authContext';
+import { db } from '@/firebaseConfig';
+import { getRoomId } from '@/utils/common';
+
+interface IMessage {
+    createdAt: Timestamp;
+    profileUrl: string;
+    senderName: string;
+    text: string;
+    userId: string;
+}
 
 const ChatRoom: FC = () => {
+    const { user } = useAuth();
     const router = useRouter();
     const item = useLocalSearchParams();
-    const [messages, setMessages] = useState([]);
+
+    const inputRef = useRef<TextInput>(null);
+    const textRef = useRef('');
+    const [messages, setMessages] = useState<IMessage[]>([]);
 
     const onBack = () => {
         router.back();
     };
+
+    const createRoomIfNotExists = async () => {
+        let roomId = getRoomId(user?.userId, item?.userId);
+
+        await setDoc(doc(db, 'rooms', roomId), {
+            roomId,
+            createdAt: Timestamp.fromDate(new Date()),
+        });
+    };
+
+    const handleSendMessage = async () => {
+        let message = textRef.current.trim();
+
+        if (!message) {
+            return;
+        }
+
+        try {
+            let roomId = getRoomId(user?.userId, item?.userId);
+            const docRef = doc(db, 'rooms', roomId);
+
+            const messagesRef = collection(docRef, 'messages');
+
+            textRef.current = '';
+            inputRef.current?.clear();
+
+            await addDoc(messagesRef, {
+                createdAt: Timestamp.fromDate(new Date()),
+                profileUrl: user?.profileUrl,
+                senderName: user?.username,
+                text: message,
+                userId: user?.userId,
+            });
+        } catch (error: any) {
+            Alert.alert('Message', error.message);
+        }
+    };
+
+    useEffect(() => {
+        createRoomIfNotExists();
+
+        let roomId = getRoomId(user?.userId, item?.userId);
+        const docRef = doc(db, 'rooms', roomId);
+        const messageRef = collection(docRef, 'messages');
+        const q = query(messageRef, orderBy('createdAt', 'asc'));
+
+        let unSub = onSnapshot(q, (snapshot) => {
+            let allMessages = snapshot.docs.map((doc) => {
+                return doc.data();
+            });
+
+            setMessages([...allMessages] as IMessage[]);
+        });
+
+        return unSub;
+    }, []);
 
     return (
         <CustomKeyboard inChat>
@@ -27,7 +99,7 @@ const ChatRoom: FC = () => {
 
                 <View className={'bg-neutral-100 flex-1 justify-between overflow-visible'}>
                     <View className={'flex-1'}>
-                        <MessageList />
+                        <MessageList currentUser={user} messages={messages} />
                     </View>
 
                     <View className={'pt-2'} style={{ marginBottom: hp(1.7) }}>
@@ -39,13 +111,16 @@ const ChatRoom: FC = () => {
                             >
                                 <TextInput
                                     className={'flex-1 mr-2'}
+                                    onChangeText={(value) => (textRef.current = value)}
                                     placeholder={'Type message ...'}
+                                    ref={inputRef}
                                     style={{ fontSize: hp(2), paddingVertical: 0 }}
                                 />
 
                                 <TouchableOpacity
                                     activeOpacity={0.7}
                                     className={'bg-neutral-200 mr-[1px] p-2 rounded-full'}
+                                    onPress={handleSendMessage}
                                 >
                                     <Feather color={'#737373'} name={'send'} size={hp(2.7)} />
                                 </TouchableOpacity>
--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -1,11 +1,49 @@
 import React, { FC, memo } from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+import { Timestamp } from 'firebase/firestore';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
-const MessageList: FC = () => {
+interface IMessage {
+    createdAt: Timestamp;
+    profileUrl: string;
+    senderName: string;
+    text: string;
+    userId: string;
+}
+
+interface IUser {
+    username: string;
+    profileUrl: string;
+    userId: string;
+}
+
+const MessageItem: FC<{ currentUser: IUser | null; message: IMessage }> = ({ currentUser, message }) => (
+    <>
+        {currentUser?.userId === message?.userId ? (
+            <View className={'flex-row justify-end mb-3 mr-3'}>
+                <View style={{ width: wp(80) }}>
+                    <View className={'bg-white border border-neutral-200 p-3 rounded-2xl self-end'}>
+                        <Text style={{ fontSize: hp(1.9) }}>{message.text}</Text>
+                    </View>
+                </View>
+            </View>
+        ) : (
+            <View className={'mb-3 mb-3'} style={{ width: wp(80) }}>
+                <View className={'bg-indigo-100 border border-indigo-200 p-3 px-4 rounded-2xl self-start'}>
+                    <Text style={{ fontSize: hp(1.9) }}>{message.text}</Text>
+                </View>
+            </View>
+        )}
+    </>
+);
+
+const MessageList: FC<{ currentUser: IUser | null; messages: IMessage[] }> = ({ currentUser, messages }) => {
     return (
-        <View>
-            <Text>MessageList</Text>
-        </View>
+        <ScrollView contentContainerStyle={{ paddingTop: 10 }} showsVerticalScrollIndicator={false}>
+            {messages.map((message, index) => (
+                <MessageItem currentUser={currentUser} key={`Message-${index}`} message={message} />
+            ))}
+        </ScrollView>
     );
 };
 
